741d607cf0027c24754045e31a05fb95871c5105
Ebru BAYRAM <133230330+aebayram@users.noreply.github.com>
2023-06-21 02:33:11 +0300

    CoffeeApp
    CoffeeApp

---
+++ Menu.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeApp
+{
+    public class Menu
+    {
+
+        public void yemekMenüsü()
+        {
+            Console.WriteLine("YEMEKLER");
+            Console.WriteLine("1) sandiviçler");
+            Console.WriteLine("2) pasta");
+            Console.WriteLine("3) cheescake");
+            Console.WriteLine("4) cookie");
+            Console.WriteLine("5) atistirmaliklar");
+            Console.WriteLine("---------------------");
+        }
+
+        public void içecekMenüsü()
+        {
+            Console.WriteLine("---------------------");
+            Console.WriteLine("İÇECEKLER");
+            Console.WriteLine("6) Turk Kahvesi");
+            Console.WriteLine("7) Su");
+            Console.WriteLine("8) Çay");
+            Console.WriteLine("9) Soguk Içecekler");
+            Console.WriteLine("10) Filtre Kahve");
+            Console.WriteLine("11) Sicak Cikolata");
+        }
+
+        public void kredikartı()
+        {
+            Console.WriteLine("kart bilgilerinizi giriniz...\nÖdemeniz Onaylandı");
+            Console.WriteLine("Afiyet Olsun Iyi Gunler");
+        }
+
+        public double nakit(double alınanpara, double hesap)
+        {
+            double paraüstü = alınanpara - hesap;
+            return paraüstü;
+        }
+
+    }
+
+
+
+
+    public class Fiyatliste
+    {
+
+        public double atıştırmalıklar = 18;
+        public double cheescake = 35;
+        public double cookie = 25;
+        public double pasta = 40.5;
+        public double sandiviçler = 46.45;
+
+        public double filtekahve = 24.5;
+        public double su = 8;
+        public double sıcakçikolata = 24.5;
+        public double türkkahvesi = 22.5;
+        public double çay = 15;
+        public double soğukiçecekler = 20;
+
+
+
+    }
+
+
+}
+
+
+
+
+
---
+++ Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu Menü1 = new Menu();
+            Fiyatliste YemekİçecekListe = new Fiyatliste();
+            double[] toplamücret = new double[9];
+
+
+            Console.WriteLine("Deniz Cafeye Hoş Geldiniz");
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("MENÜYÜ GÖRMEK İÇİN 'M' tuşuna basiniz");
+            string seçim = Console.ReadLine().ToUpper();
+            Console.Clear();
+            if (seçim == "M")
+            {
+                Menü1.yemekMenüsü();
+                Menü1.içecekMenüsü();
+            }
+
+
+
+        ürünler:
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("Lutfen urun seciniz");
+            seçim = Console.ReadLine();
+
+
+
+            switch (seçim)
+            {
+                case "1":
+                    toplamücret[0] = YemekİçecekListe.sandiviçler;
+
+                    break;
+                case "2":
+
+                    toplamücret[1] = YemekİçecekListe.pasta;
+                    break;
+                case "3":
+
+                    toplamücret[2] = YemekİçecekListe.cheescake;
+                    break;
+                case "4":
+
+                    toplamücret[3] = YemekİçecekListe.cookie;
+                    break;
+                case "5":
+
+                    toplamücret[4] = YemekİçecekListe.atıştırmalıklar;
+                    break;
+                case "6":
+
+                    toplamücret[5] = YemekİçecekListe.türkkahvesi;
+                    break;
+                case "7":
+
+                    toplamücret[6] = YemekİçecekListe.su;
+                    break;
+                case "8":
+
+                    toplamücret[7] = YemekİçecekListe.çay;
+                    break;
+                case "9":
+
+                    toplamücret[8] = YemekİçecekListe.soğukiçecekler;
+                    break;
+                case "10":
+
+                    toplamücret[9] = YemekİçecekListe.filtekahve;
+                    break;
+                case "11":
+
+                    toplamücret[10] = YemekİçecekListe.sıcakçikolata;
+                    break;
+
+                default:
+                    Console.WriteLine("LÜTFEN GEÇERLİ BİR NUMARA GİRİN!!!");
+                    break;
+
+
+
+
+            }
+
+            Console.WriteLine("yeni urun eklemek ister misiniz?\nEvet ise 'E' Hayir ise 'H' seçiniz...");
+
+            seçim = Console.ReadLine().ToUpper();
+
+            if (seçim == "E")
+            {
+                goto ürünler;
+            }
+            else
+            {
+                double toplam = 0;
+                for (int i = 0; i < toplamücret.Length; i++)
+                {
+                    toplam += toplamücret[i];
+                }
+
+                Console.Clear();
+                Console.WriteLine(toplam + "tl");
+
+                Console.WriteLine("Odeme Turunuzu seciniz\n1)Kredi Kartı\n2)Nakit");
+                seçim = Console.ReadLine();
+
+                Console.Clear();
+
+                if (seçim == "1")
+                {
+                    Menü1.kredikartı();
+                }
+                else if (seçim == "2")
+                {
+                    Console.WriteLine("nakit miktarini giriniz");
+                    double alınanpara = double.Parse(Console.ReadLine());
+                    double paraüstü = Menü1.nakit(alınanpara, toplam);
+                    Console.WriteLine("para ustunuz=" + paraüstü + "tl");
+                    Console.WriteLine("Afiyet Olsun iyi Gunler...");
+                }
+
+
+            }
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
+
+
+
+
